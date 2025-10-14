d728edf25b8626fd8e7cc0fcaf800f2c55436784
Tiago Jesus <tjesus@idmind.pt>
2025-10-14 18:27:06 +0100

    fix route

--- APIV2.py
+++ APIV2.py
@@ -207,7 +207,7 @@ def check_pairing():
     time.sleep(0.25)
     return jsonify({'paired': webapp.IsPaired}), 200
 
-@app.route('/chech_pair_state')
+@app.route('/check_pair_state')
 def chech_pair_state():
     time_last_msg = time.time() - gps_points.last_gps_time
     if time_last_msg > 60 * 15 or not webapp.IsPaired: # 15 minutes wihtout messages or isnt even paired
